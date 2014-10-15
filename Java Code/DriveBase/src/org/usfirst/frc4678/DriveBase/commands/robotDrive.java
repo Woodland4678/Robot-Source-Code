d2dc5a9f2e4a272ebbcbd70c9c23dea596119d11
Woodland4678 <robotics@woodland.on.ca>
2014-10-15 16:22:05 -0400

    Debug Class

--- Java Code/DriveBase/src/org/usfirst/frc4678/DriveBase/commands/robotDrive.java
+++ Java Code/DriveBase/src/org/usfirst/frc4678/DriveBase/commands/robotDrive.java
@@ -29,6 +29,7 @@ public class  robotDrive extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.logger.info("robotDrive", "initialized");
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
