3f894bb2e2307ed14a4572e11b7c959d0cc6d7b0
Woodland4678 <robotics@woodland.on.ca>
2015-03-02 16:00:19 -0500

    Added new setarm command, renmoved PID control of arm

--- 2015 Robot Code/walle/src/org/usfirst/frc4678/walle/commands/OpenArm2.java
+++ 2015 Robot Code/walle/src/org/usfirst/frc4678/walle/commands/OpenArm2.java
@@ -37,8 +37,7 @@ public class  OpenArm2 extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.pIDSubsystem1.setSetpoint(0.75);
-    	Robot.pIDSubsystem1.enable();
+    	Robot.squeeze.openArm(Robot.armOpenPosition());
     	
     }
 
@@ -49,12 +48,10 @@ public class  OpenArm2 extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.pIDSubsystem1.disable();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.pIDSubsystem1.disable();
     }
 }
